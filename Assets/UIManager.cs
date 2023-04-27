dd27daf8acb22478ff743da2c40168bb59e486f9
XavierC21 <123001920+XavierC21@users.noreply.github.com>
2023-04-26 20:32:45 -0400

    Animated both buttons

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -5,10 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    public Animator startButton;
+    public Animator settingsButton;
+
     // Start is called before the first frame update
     public void StartGame()
     {
         SceneManager.LoadScene("RocketMouse");
+    }
+
+    public void OpenSettings()
+    {
+        startButton.SetBool("isHidden", true);
+        settingsButton.SetBool("isHidden", true);
     }
 
     // Update is called once per frame
